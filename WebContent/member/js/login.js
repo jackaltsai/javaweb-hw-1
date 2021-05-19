c9499275ba13b2b84c6257863e9c8edc6b511d7d
陳柏文 <bbtiger0826@gmail.com>
2021-05-19 10:36:42 +0800

    Revert "新增login 登入頁面"
    This reverts commit cc3ee41817973a0185f5807589cf3efd4f5a1739.

--- WebContent/member/js/login.js
+++
@@ -1,9 +0,0 @@
-const btnsignin = document.getElementById('btnsignin');
-btnsignin.onclick = () => {
-    fetch("../signin")
-	.then(resp => resp.json())
-	.then(memberList => {
-		console.log(memberList);
-	});
-};
-
--- WebContent/member/js/register.js
+++
@@ -1,9 +0,0 @@
-const btnregister = document.getElementById('btnregister');
-btnregister.onclick = () => {
-    fetch("../register")
-	.then(resp => resp.json())
-	.then(memberList => {
-		console.log(memberList);
-	});
-};
-
