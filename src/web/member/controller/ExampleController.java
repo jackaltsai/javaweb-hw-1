b5efdff0e1ef409cb8179b912a1cc9a8cb4284f8
陳柏文 <bbtiger0826@gmail.com>
2021-05-19 13:25:14 +0800

    更新BUG - Servlet網址錯誤

--- src/web/member/controller/ExampleController.java
+++ src/web/member/controller/ExampleController.java
@@ -9,8 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import web.member.service.MemberService;
 
-// @WebServlet("/ExampleController")
-@WebServlet("member/example")
+@WebServlet("/member/example")
 public class ExampleController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	// Service層
