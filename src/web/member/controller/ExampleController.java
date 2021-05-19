36cf0357954daf4e14f6e17a1f1574f9747c098e
陳柏文 <bbtiger0826@gmail.com>
2021-05-19 08:52:41 +0800

    v 1.0.0 - 更改ExampleController的網址導向

--- src/web/member/controller/ExampleController.java
+++ src/web/member/controller/ExampleController.java
@@ -9,7 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import web.member.service.MemberService;
 
-@WebServlet("/ExampleController")
+// @WebServlet("/ExampleController")
+@WebServlet("member/example")
 public class ExampleController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	// Service層
