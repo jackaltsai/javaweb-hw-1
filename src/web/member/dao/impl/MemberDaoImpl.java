75ea968ac2deaef3c34f07239be8a297b5920c0d
林以樂 <a0989758574@gmail.com>
2021-05-19 10:29:16 +0800

    insert. 5/19

--- src/web/member/dao/impl/MemberDaoImpl.java
+++ src/web/member/dao/impl/MemberDaoImpl.java
@@ -1,19 +1,45 @@
 package web.member.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 import web.member.bean.Member;
 import web.member.dao.MemberDao;
 
 public class MemberDaoImpl implements MemberDao {
-
+	private DataSource ds;
+	
+	public MemberDaoImpl() {
+		try {
+			ds = (DataSource) new InitialContext().lookup("java:/comp/env/jdbc/example");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
     @Override
     public int insert(Member member) {
-        
-        // 錯誤代碼 -1 回傳
-        return -1;
-    }
-
+    	final String sql = "insert into MEMBER_Test(ACCOUNT,PASSWORD,NICKNAME) values (?,?,?)";
+//		Member member2 = new Member();
+		try (
+			Connection conn = ds.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(sql)
+			){
+//				pstmt.setString(1, member2.getAccount());
+//				pstmt.setString(2, member2.getPassword());
+//				pstmt.setString(3, member2.getNickname());
+			pstmt.setString(1, "Emilia tan");
+			pstmt.setString(2, "test123");
+			pstmt.setString(3, "emt");
+				return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1;
+	}
     @Override
     public int deleteById(Integer id) {
         
--- src/web/member/service/MemberService.java
+++ src/web/member/service/MemberService.java
@@ -1,13 +1,19 @@
 package web.member.service;
 
+import web.member.bean.Member;
 import web.member.dao.MemberDao;
 import web.member.dao.impl.MemberDaoImpl;
 
 public class MemberService {
     private MemberDao dao;
+    Member member = new Member();
     
     public MemberService() {
         // 父介面 <- 子實作類別
         dao = new MemberDaoImpl();
+    }
+    
+    public int insert() {
+    	return dao.insert(member);
     }
 }
