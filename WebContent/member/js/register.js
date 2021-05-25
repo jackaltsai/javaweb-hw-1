2798d878785fde4de58dd29ebca45419316947a6
jackaltsai <hata.s520@gmail.com>
2021-05-25 19:22:42 +0800

    新增register頁面 帳號長度5~50

--- WebContent/member/js/register.js
+++ WebContent/member/js/register.js
@@ -1,10 +1,17 @@
-const btnregister = document.getElementById('btnregister');
-btnregister.onclick = () => {
-    fetch("../register")
-	.then(resp => resp.json())
-	.then(memberList => {
-		console.log(memberList);
-	});
-};
-
+function checkUserid(){
+	var ueridObj=document.getElementById("userid");
+	var userid=trim(ueridObj.value);
+	var useridRegex= /^\w{5,50}$/;
+	var msg ="   <img src='img/true.gif' style='width:12px'>";
+	if(!userid)
+		msg ="<font color='red'>账号不能为空！</font>";
+	else if(!useridRegex.test(userid))
+		msg ="<font color='red'>账号长度不对！</font>";
+	var span = document.getElementById("useridSpan");
+	span.innerHTML = msg;
+	return msg == "   <img src='img/true.gif' style='width:12px'>";
+	}
+function trim(s){
+   return s.replace(/^\s+|\s+$/g,"");
+}
 
