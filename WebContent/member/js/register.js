8df46a6bbb9545d8aac47f07f08157c557131ca8
林以樂 <a0989758574@gmail.com>
2021-05-26 20:19:05 +0800

    no message

--- WebContent/member/js/register.js
+++ WebContent/member/js/register.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-function checkUserid(){
-	var ueridObj=document.getElementById("floatingInput");
-	var floatingInput=trim(ueridObj.value);
-	var useridRegex= /^\w{5,50}$/;
-	var msg ="   <a style='width:12px'>X</a>";
-	if(!floatingInput)
-		msg ="<font color='red'>帳號不能為空！</font>";
-	else if(!useridRegex.test(floatingInput))
-		msg ="<font color='red'>帳號長度不對！</font>";
-	var span = document.getElementById("useridSpan");
-	span.innerHTML = msg;
-	return msg == " O";
-}
-=======
 // const btnregister = document.getElementById('btnregister');
 // btnregister.onclick = () => {
 //     fetch("../register")
